ce384198022f67f3c1ba56e295efa2f6ccc07cc1
tarunvella <vella.tarun@gmail.com>
2020-03-29 20:43:37 +0530

    added Child component BookItem.vue to parent BookItem.vue

---
+++ src/components/BookItem.vue
@@ -0,0 +1,14 @@
+<template>
+    {{book.title}} : {{ book.author}}
+</template>
+
+<script>
+  export default {
+    name: 'BookItem',
+    props: ['book']
+  };
+</script>
+
+<style scoped>
+
+</style>
