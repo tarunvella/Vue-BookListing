f3e2cfc6d1e7838c7a8e869e5e38d60491af5158
tarunvella <vella.tarun@gmail.com>
2020-03-30 14:53:59 +0530

    v-model directive for bookTitle property

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" action="#" method="post">
-    <input type="text" name="title" placeholder="Book Title">
+    <input v-model="bookTitle" type="text" name="title" placeholder="Book Title">
     <input type="text" name="author" placeholder="Book Author">
     <button type="submit" >Add Book</button>
   </form>
