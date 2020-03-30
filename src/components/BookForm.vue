4e14466d9e26676d910c497b783d1106c2372a03
tarunvella <vella.tarun@gmail.com>
2020-03-30 14:35:40 +0530

    Added a form to a BookForm.vue template

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <form action="#" method="post">
+    <input type="text" name="title" placeholder="Book Title">
+    <input type="text" name="author" placeholder="Book Author">
+    <button type="submit" name="button">Add Book</button>
+  </form>
 </template>
 
 <script>
